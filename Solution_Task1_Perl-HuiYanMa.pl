471f9dbf1c0df4ed90a146817aa1616359b37e38
mhy35706 <mhy35706@sina.com>
2017-04-19 08:30:32 -0700

    Add files via upload
    Task 1. Write a script that will parse data from the tabledata.csv file, then create and insert the csv data into a table in a mysql database (connection details below).
     
    Task 2. Write a 2nd script that will query the table from task 1, and calculate and display the average of all values from column "temp".
    
    Database Connection Info:
     
    Server: sql3.freemysqlhosting.net
     Name: sql3169774
     Username: sql3169774
     Password: GdsGb54Lky
     Port number: 3306​

---
+++ Solution_Task1_Perl-HuiYanMa.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+use strict;
+use DBI;
+use Text::CSV;
+
+my $file = "tabledata.csv";
+
+open CSV, $file or die "Couldn't open file $file, $!\n";
+my @csv_content = <CSV>;
+close CSV;
+
+my $dbname = "sql3169774";
+my $dbhost = "sql3.freemysqlhosting.net";
+my $dbport = "3306";
+my $dbuser = "sql3169774";
+my $dbpass = "GdsGb54Lky";
+my $table = "TableTest";
+
+my $dsn = "DBI:mysql:database=$dbname;host=$dbhost;port=$dbport;";
+my $dbh = DBI->connect($dsn, $dbuser, $dbpass) or or die "Couldn't connect to database: " . DBI->errstr;
+
+my $sth;
+my $statement;
+
+my $field_line = shift(@csv_content);
+chomp($field_line);
+
+my @values;
+my $status;
+my $line;
+
+my $csv = Text::CSV->new();
+foreach(@csv_content){
+   if ($csv->parse($_)){
+     @values = $csv->fields();
+      if ($status = $csv->combine(@values)){
+         $line = $csv->string();
+
+         $statement = "INSERT INTO $table ($field_line) VALUES ($line);";
+         print $statement."\n";
+         $sth = $dbh->prepare($statement) or die "Couldn't prepare statement: " . $dbh->errstr\n;
+         $sth->execute() or die "Couldn't execute statement: " . $sth->errstr\n;
+
+      }
+      else {
+          my $bad_arg1 = $csv->error_input ();
+	  print "Failed to combine values\n";
+
+      }
+    }
+	
+   else {
+        my $bad_arg2 = $csv->error_input ();
+	print "Failed to Parse line\n";
+    }
+
+    if (not $cvs->eof){
+	$csv->error_diag();
+    }
+}
